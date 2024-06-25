71e51ddb01ace7501329243b2766fee875b54609
JeetDas5 <jeet15083011@gmail.com>
2024-06-25 20:59:03 +0530

    changes in restapi, urls, views for pylint

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -12,11 +12,13 @@ sentiment_analyzer_url = os.getenv(
     default="http://localhost:5050/")
 
 # def get_request(endpoint, **kwargs):
+
+
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+key+"="+value+"&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -31,6 +33,8 @@ def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 
 # def analyze_review_sentiments(text):
+
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
@@ -45,10 +49,12 @@ def analyze_review_sentiments(text):
 
 # def post_review(data_dict):
 # Add code for posting review
+
+
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
